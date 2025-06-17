c71b92a33e991dfbcf1601135c69f283abd32147
dinabandhu2004 <dinabandhu2004barai@gmail.com>
2025-06-17 11:50:17 +0530

    layout done.

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,18 @@ function App() {
 
   return (
     <>
-      
+      <div className='navBar'></div>
+      <div className='homeSection'>
+        <div className='homeConText'></div>
+        <div className='homeConImg'></div>
+      </div>
+      <div className="aboutSection">
+        <div className='aboutConImg'></div>
+        <div className='aboutConText'></div>
+      </div>
+      <div className="skillSection"></div>
+      <div className="projectSection"></div>
+      <div className="contactSection"></div>
     </>
   )
 }
