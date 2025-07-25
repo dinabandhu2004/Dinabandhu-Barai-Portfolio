35bf539eb323ea95241cfab447c0c663c276dc98
dinabandhu2004 <dinabandhu2004barai@gmail.com>
2025-07-25 21:59:53 +0530

    responsive done

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,28 @@
 import { useState } from 'react'
 import './nav.css'
-function Nav() {
- const [color,setcolor] = useState('black');
+import { FiMoreVertical } from "react-icons/fi"; // install react-icons if needed
 
+function Nav() {
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <div className='nav'>
-        <div><span style={{color:'#291baf'}}>D</span>inabandhu <span style={{color:'#291baf'}}>B</span>arai</div>
-        <div className='navRoute'>
-            <button className='navBtn' ><a className="navBtn" href="#home">Home</a></button>
-            <button className='navBtn'><a className="navBtn" href="#about">About</a></button>
-            <button className='navBtn'><a className="navBtn" href="#skills">Skills</a></button>
-            <button className='navBtn'><a className="navBtn" href="#project">Projects</a></button>
-            <button className='navBtn'><a className="navBtn" href="#contact">Contact</a></button>
-        </div>
+      <div className='nav-logo'>
+        <span style={{ color: '#291baf' }}>D</span>inabandhu <span style={{ color: '#291baf' }}>B</span>arai
+      </div>
+
+      {/* Hamburger (three-dot) icon */}
+      <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+        <FiMoreVertical size={32} />
+      </div>
+
+      <div className={`navRoute ${menuOpen ? 'open' : ''}`}>
+        <button className='navBtn'><a className="navBtn" href="#home">Home</a></button>
+        <button className='navBtn'><a className="navBtn" href="#about">About</a></button>
+        <button className='navBtn'><a className="navBtn" href="#skills">Skills</a></button>
+        <button className='navBtn'><a className="navBtn" href="#project">Projects</a></button>
+        <button className='navBtn'><a className="navBtn" href="#contact">Contact</a></button>
+      </div>
     </div>
   )
 }
