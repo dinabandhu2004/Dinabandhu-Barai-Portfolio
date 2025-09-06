c6a6c4db10d77b3bf36b0e2b91beebd3b0d5edbb
dinabandhu2004 <dinabandhu2004barai@gmail.com>
2025-09-06 15:26:44 +0530

    type animation added

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -1,22 +1,48 @@
 import './HomeContent.css'
-import { FaPhone,FaEnvelope, FaLinkedin, FaGithub, FaInstagram, FaCheckCircle } from 'react-icons/fa';
+import { FaEnvelope, FaLinkedin, FaGithub } from 'react-icons/fa';
+import { ReactTyped } from 'react-typed';
 
 const HomeContent = () => {
   return (
     <div className="content">
       <div className='fontSmall'>Hello! I am</div>
       <div className='fontBig'>Dinabandhu Barai</div>
-      <div className='fontSmall'>FullStack Developer</div>
-      <button className='fontSmall btn-col'><a href="#contact" style={{ textDecoration: 'none', backgroundColor: "#291baf" }}>Contact</a></button>
-      <div className='Homelinks'>
-        <div><a href="https://github.com/dinabandhu2004" target="_blank" rel="noopener noreferrer"><FaGithub/></a></div>
-        <div><a href="https://www.linkedin.com/in/dinabandhu-barai-802668244/" target="_blank" rel="noopener noreferrer"><FaLinkedin/></a></div>
-        <div><a href="mailto:dinabandhu2004barai@gmail.com"><FaEnvelope/></a></div>
+      <div className='fontSmall typing'>
+        <ReactTyped
+          strings={[
+            "FullStack Developer",
+            "Software Developer",
+            "Programmer"
+          ]}
+          typeSpeed={80}
+          backSpeed={50}
+          loop
+        />
       </div>
-
-
+      <button className='fontSmall btn-col'>
+        <a href="#contact" style={{ textDecoration: 'none', backgroundColor: "#291baf" }}>
+          Contact
+        </a>
+      </button>
+      <div className='Homelinks'>
+        <div>
+          <a href="https://github.com/dinabandhu2004" target="_blank" rel="noopener noreferrer">
+            <FaGithub/>
+          </a>
+        </div>
+        <div>
+          <a href="https://www.linkedin.com/in/dinabandhu-barai-802668244/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin/>
+          </a>
+        </div>
+        <div>
+          <a href="mailto:dinabandhu2004barai@gmail.com">
+            <FaEnvelope/>
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default HomeContent
+export default HomeContent;
